a19c0357e1a1ef919abdd8d3320444e4d037f5a9
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-12-18 16:42:00 +0530

    api call using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import React from "react"
+import fetchDataFromApi from "./utils/api"
+
 
 
 function App() {
+
+  const apiCall = () => {
+    fetchDataFromApi("/movie/popular")
+    .then((data) => console.log(data))
+  }
+
+  React.useEffect(() => {
+    apiCall()
+  }, [])
 
   return (
     <>
---
+++ src/utils/api.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const baseURL = "https://api.themoviedb.org/3"
+
+const token = import.meta.env.VITE_APP_TMDB_TOKEN
+
+const header = {
+    Authorization: `Bearer ${token}`
+}
+
+const fetchDataFromApi = async (url, params) => {
+    try {
+
+        const data = await axios.get(baseURL + url, {
+            headers: header,
+            params: params
+        })
+
+        return data.data
+
+    } catch (error) {
+        console.log(error)
+        return error
+    }
+
+}
+
+export default fetchDataFromApi
