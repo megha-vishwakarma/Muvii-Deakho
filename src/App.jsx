182bdad40485e00cf88960f9ae00853ae9d3539d
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-12-19 17:14:08 +0530

    redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import React from "react"
 import fetchDataFromApi from "./utils/api"
-
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfigration} from "./features/homeSlice"
 
 
 function App() {
 
+  const dispatch = useDispatch()
+  const home = useSelector((state) => state.home)
+
   const apiCall = () => {
     fetchDataFromApi("/movie/popular")
-    .then((data) => console.log(data))
+    .then((data) => {console.log(data)
+      dispatch(getApiConfigration(data))
+    })
   }
 
   React.useEffect(() => {
     apiCall()
+    
   }, [])
 
   return (
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from '../features/counter/counterSlice.js'
+import homeSlice from "../features/homeSlice";
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        home: homeSlice
+    },
 });
 
 export default store;
