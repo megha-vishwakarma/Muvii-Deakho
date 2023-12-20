2217c79320ce0fe283c2e637b280abc8a47619ed
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-12-20 11:46:02 +0530

    react router setup added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@ import React from "react"
 import fetchDataFromApi from "./utils/api"
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfigration} from "./features/homeSlice"
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/home/HomePage"
+import Details from "./pages/details/Details"
+import SearchResult from "./pages/home/searchResult/SearchResult"
+import ErrorSection from "./pages/404/ErrorSection"
+import Footer from "./components/footer/Footer"
+import Header from "./components/header/Header"
 function App() {
 
   const dispatch = useDispatch()
@@ -22,9 +27,16 @@ function App() {
   }, [])
 
   return (
-    <>
-      <div >hello</div>
-    </>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/"  element = {<HomePage/>}/>
+        <Route path="/:mediaType/:id"  element = {<Details/>}/>
+        <Route path = "/search/:searchQuery" element = {<SearchResult/>}/>
+        <Route path = "*" element = {ErrorSection}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
