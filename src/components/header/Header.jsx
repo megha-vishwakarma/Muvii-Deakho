5826c2c007b172238a091126460adf5aaac7f239
khan-mujeeb <mujeebkhan1831@gmail.com>
2024-01-03 01:05:43 +0530

    nav bar added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
+import "./Header.scss";
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.png";
 
-function Header() {
-  return (
-    <div>header</div>
-  )
-}
+const Header = () => {
+    const [show, setShow] = useState("top");
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const [query, setQuery] = useState("");
+    const [showSearch, setShowSearch] = useState("");
+    const navigate = useNavigate();
+    const location = useLocation();
 
-export default Header
+    const openSearch = () => {
+        setMobileMenu(false)
+        setShowSearch(true)
+    };
+
+    const openMobileMenu = () => {
+        setMobileMenu(true)
+        setShowSearch(false)
+    };
+
+    return <header className={`header ${mobileMenu?"mobileView":""} ${show}`}>
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+
+        <ul className="menuItems">
+          <li className="menuItem">Movies</li>
+          <li className="menuItem">TV Shows</li>
+          <li className="menuItem">
+            <HiOutlineSearch />
+          </li>
+        </ul>
+
+        <div className="mobileMenuItems ">
+          <HiOutlineSearch />
+          {
+            mobileMenu ? <VscChromeClose onClick={() => setMobileMenu()}/> : <SlMenu onClick={() => openMobileMenu()}/>
+          }
+        </div>
+      </ContentWrapper>
+    </header>;
+};
+
+export default Header;
