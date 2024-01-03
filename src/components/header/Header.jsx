d244ced67bfe09256afa891a5b01b01f86c88174
khan-mujeeb <mujeebkhan1831@gmail.com>
2024-01-03 22:06:00 +0530

    nav bar added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,39 +15,73 @@ const Header = () => {
     const [showSearch, setShowSearch] = useState("");
     const navigate = useNavigate();
     const location = useLocation();
+    const [searchQuery, setSearchQuery] = React.useState("");
 
     const openSearch = () => {
-        setMobileMenu(false)
-        setShowSearch(true)
+        setMobileMenu(false);
+        setShowSearch(true);
+    };
+
+    const searchQueryHandler = (event) => {
+        if (event.key === "Enter" && searchQuery !== "") {
+            navigate(`/search/${searchQuery}}`);
+        }
+
+        setTimeout(() => {
+            setShowSearch(false);
+        }, 1000);
     };
 
     const openMobileMenu = () => {
-        setMobileMenu(true)
-        setShowSearch(false)
+        setMobileMenu(true);
+        setShowSearch(false);
     };
 
-    return <header className={`header ${mobileMenu?"mobileView":""} ${show}`}>
-      <ContentWrapper>
-        <div className="logo">
-          <img src={logo} alt="logo" />
-        </div>
+    return (
+        <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+            <ContentWrapper>
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                </div>
 
-        <ul className="menuItems">
-          <li className="menuItem">Movies</li>
-          <li className="menuItem">TV Shows</li>
-          <li className="menuItem">
-            <HiOutlineSearch />
-          </li>
-        </ul>
+                <ul className="menuItems">
+                    <li className="menuItem">Movies</li>
+                    <li className="menuItem">TV Shows</li>
+                    <li className="menuItem">
+                        <HiOutlineSearch />
+                    </li>
+                </ul>
 
-        <div className="mobileMenuItems ">
-          <HiOutlineSearch />
-          {
-            mobileMenu ? <VscChromeClose onClick={() => setMobileMenu()}/> : <SlMenu onClick={() => openMobileMenu()}/>
-          }
-        </div>
-      </ContentWrapper>
-    </header>;
+                <div className="mobileMenuItems ">
+                    <HiOutlineSearch onClick={() => openSearch()} />
+                    {mobileMenu ? (
+                        <VscChromeClose onClick={() => setMobileMenu()} />
+                    ) : (
+                        <SlMenu onClick={() => openMobileMenu()} />
+                    )}
+                </div>
+            </ContentWrapper>
+            {showSearch && (
+                <div className="searchBar">
+                    <ContentWrapper>
+                        <div className="searchInput">
+                            <input
+                                onChange={(e) => {
+                                    setSearchQuery(e.target.value);
+                                }}
+                                onKeyUp={searchQueryHandler}
+                                type="text"
+                                placeholder="Search for a movie or tv show...."
+                            />
+                            <VscChromeClose
+                                onClick={() => setShowSearch(false)}
+                            />
+                        </div>
+                    </ContentWrapper>
+                </div>
+            )}
+        </header>
+    );
 };
 
 export default Header;
